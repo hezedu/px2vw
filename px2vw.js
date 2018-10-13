e4e94d48401ababe38ba3d2551b3382d8f862d59
Du Wei <hezedu@hotmail.com>
2018-10-13 11:36:58 +0800

    Update px2vw.js

--- px2vw.js
+++ px2vw.js
@@ -1,6 +1,6 @@
 var WIDTH = 320;
 //,FIXED = 5;
-
+const minVw = (1 / 320) * 100;
 var REG = /([1-9]\d*\.\d*|0\.\d*[1-9]|\d)+px(?!(\s*\)))/gi; //去零正则表达式
 // function trimEnd0(str) { //去掉未尾多余的0.
 //   str = str.replace(/0+$/, '');
@@ -11,8 +11,15 @@ var REG = /([1-9]\d*\.\d*|0\.\d*[1-9]|\d)+px(?!(\s*\)))/gi; //去零正则表达
 function matchCtrl(width) {
   return function(m) { //replace匹配字符串处理
     m = m.substr(0, m.length - 2);
-    m = Number(m);
+    if(m[0] === '.'){
+      m = Number('0' + m);
+    }else{
+      m = Number(m);
+    }
     m = (m / width) * 100;
+    if(m < minVw){
+      m = minVw;
+    }
     //m = m.toFixed(fixed);
     //return trimEnd0(m.toString()) + 'vw';
     return m + 'vw';
