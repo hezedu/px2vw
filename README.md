61a1d1f370125172170d39950d561da39950b92c
tianxiazhizhong <tianxiazhizhong@gmail.com>
2018-11-20 22:43:02 +0800

    up 1.1.2

--- README.md
+++ README.md
@@ -3,13 +3,13 @@ css's px to vw. online edit:[https://hezedu.github.io/px2vw/](https://hezedu.git
 ### install
 `npm install px2vw`
 ## API
-### px2vw(str, content)
+### px2vw(width, content, minWidth)
 
-- `width`  <**Number**> 客户端宽度(px)。默认`320`.
-- `content`  <**String**> css源文件内容
-
+- `width`  <**Number**> 客户端宽度(px)。默认`320`。
+- `content`  <**String**> css源文件内容。
+- `minWidth` <**Number**> 可选参数。客户端最小宽度(px)。默认`320`。如果 vw 单位太小将会不显示。 比如 0.0125vw 在 320 屏上不显示。故设此参数。(1.2.2 更新)
 ### example
 ```js
-var result = px2vw(414, '.title: height:30px');
+var result = px2vw(414, '.title: height:30px; border:1px solid red;', 320);
 console.log(result);
 ```
--- px2vw.js
+++ px2vw.js
@@ -1,14 +1,8 @@
 var WIDTH = 320;
-//,FIXED = 5;
-const minVw = (1 / 320) * 100;
-var REG = /([1-9]\d*\.\d*|0\.\d*[1-9]|\d)+px(?!(\s*\)))/gi; //去零正则表达式
-// function trimEnd0(str) { //去掉未尾多余的0.
-//   str = str.replace(/0+$/, '');
-//   var lastIndex = str.length - 1;
-//   return str[lastIndex] !== '.' ? str : str.substr(0, lastIndex);
-// }
 
-function matchCtrl(width) {
+var REG = /([1-9]\d*\.\d*|0\.\d*[1-9]|\d)+px(?!(\s*\)))/gi; //去零正则表达式
+
+function matchCtrl(width, minVw) {
   return function(m) { //replace匹配字符串处理
     m = m.substr(0, m.length - 2);
     if(m[0] === '.'){
@@ -20,14 +14,15 @@ function matchCtrl(width) {
     if(m < minVw){
       m = minVw;
     }
-    //m = m.toFixed(fixed);
-    //return trimEnd0(m.toString()) + 'vw';
     return m + 'vw';
   }
 }
 
-function px2vw(width, str) {
+function px2vw(width, str, minWidth) {
   width = width || WIDTH;
-  return str.replace(REG, matchCtrl(width));
+  minWidth = minWidth || WIDTH;
+  var minVw = (1 / minWidth) * 100;
+  return str.replace(REG, matchCtrl(width, minVw));
 }
+
 module.exports = px2vw;
